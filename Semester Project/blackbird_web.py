71d2a8406add1291e174b588065ee2f9a8183a2d
Luis M <77978673+OrangeXR@users.noreply.github.com>
2025-12-10 17:55:49 -0600

    Update blackbird_web.py

--- Semester Project/blackbird_web.py
+++ Semester Project/blackbird_web.py
@@ -8,16 +8,6 @@ app = Flask(__name__)
 def home_page():
     return render_template("HomePage.html")
 
-#@app.route('/search')
-#def home():
- #   return """
-  ## <form action="/search" method="get">
-    #    <label>Username: <input type="text" name="username"></label><br>
-     #   <label>Email: <input type="text" name="email"></label><br>
-      #  <input type="submit" value="Search">
-    #</form>
-    #"""
-
 @app.route('/search', methods=['POST', 'GET'])
 def search():
     username = None
@@ -33,18 +23,8 @@ def search():
     if username and email:
         results = run_blackbird_search(username=username, email=email)
 
-
-    # Render results as a list
-    #html = """
-   # <h2>Search Results</h2>
-    #<ul>
-   # {% for account in results %}
-   #     <li>{{ account['name'] }} - <a href="{{ account['url'] }}">{{ account['url'] }}</a> ({{ account['status'] }})</li>
-   # {% endfor %}
-   # </ul>
-   # <a href="/">Back</a>
-   # """
     return render_template("Search.html", results=results)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
